1d0e269bb742c48e5263b6a717a2418d1cd10755
roderickjonathon <roderickjgillies@gmail.com>
2019-07-01 15:46:35 +0100

    graph leaderboard in place

--- client/src/components/Leaderboard.vue
+++ client/src/components/Leaderboard.vue
@@ -2,7 +2,7 @@
   <div>
     <div>
       {{arrayOfFootprints()}}
-      <apexcharts width="500" type="bar" :options="chartOptions" :series="series"></apexcharts>
+      <apexcharts id="leaderboard" width="500" type="bar" :options="chartOptions" :series="series"></apexcharts>
     </div>
 
 
@@ -32,11 +32,11 @@ export default {
             id: 'vuechart-example'
           },
           xaxis: {
-            categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+            categories: [this.arrayOfNames().join(" ")]
           }
         },
         series: [{
-          name: 'series-1',
+
           data: this.arrayOfFootprints()
         }]
       }
@@ -74,6 +74,13 @@ export default {
     arrayOfFootprints(){
       const result = this.profiles.map( (profile) => {
         return profile.totalCarbon
+      })
+      return result
+    },
+
+    arrayOfNames(){
+      const result = this.profiles.map( (profile) => {
+        return profile.name
       })
       return result
     }
